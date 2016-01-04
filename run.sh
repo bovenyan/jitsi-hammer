e751d8df656e4b62d92512fa77817ee9a0c4c87b
Bo Yan VM <boven.yan@gmail.com>
2016-01-04 07:34:28 -0500

    nothing modified

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./jitsi-hammer.sh -XMPPdomain boven-cute.dev.local -XMPPhost localhost -MUCdomain conference.boven-cute.dev.local -users 1 -room $1 -ivf resources/big-buck-bunny_trailer_track1_eng.ivf
+./jitsi-hammer.sh -XMPPdomain boven-cute.dev.local -XMPPhost boven-cute.dev.local -MUCdomain conference.boven-cute.dev.local -users 1 -room $1 -ivf resources/big-buck-bunny_trailer_track1_eng.ivf > jitsiHammer.log 2>&1
